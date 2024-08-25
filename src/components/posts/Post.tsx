5cf2b9b37b3f81f4384bb3328519635c72b2b8ac
OchoKOM <martinkababo@gmail.com>
2024-08-25 22:11:21 +0100

    Delete post action

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,37 +1,57 @@
+"use client";
+
 import { PostData } from "@/lib/types";
 import Link from "next/link";
 import UserAvatar from "../UserAvatar";
 import Time from "../Time";
-
+import { useSession } from "@/app/(main)/SessionProvider";
+import PostMoreButton from "./PostMoreButton";
 
 interface PostProps {
-    post: PostData;
+  post: PostData;
 }
 
 export default function Post({ post }: PostProps) {
+  const { user } = useSession();
 
-    const timestamp = post.createdAt.getTime();
-    const now = Date.now();
-    const diffInMs = now - timestamp;
+  const timestamp = post.createdAt.getTime();
+  const now = Date.now();
+  const diffInMs = now - timestamp;
 
-    const relative = (diffInMs < Math.abs(48 * 3600 * 1000))
+  const relative = diffInMs < Math.abs(48 * 3600 * 1000);
 
-    return <article className="flex flex-col p-5 rounded-2xl bg-card shadow-sm gap-5">
+  return (
+    <article className="group/post flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flex justify-between gap-3">
         <div className="flex flex-wrap gap-3">
-            <Link href={`/users/${post.user.username}`}>
-                <UserAvatar avatarUrl={post.user.avatarUrl} />
+          <Link href={`/users/${post.user.username}`}>
+            <UserAvatar avatarUrl={post.user.avatarUrl} />
+          </Link>
+          <div>
+            <Link
+              href={`/users/${post.user.username}`}
+              className="block font-medium hover:underline"
+            >
+              {post.user.displayName}
             </Link>
-            <div>
-                <Link href={`/users/${post.user.username}`} className="block font-medium hover:underline">
-                    {post.user.displayName}
-                </Link>
-                <Link href={`/posts/${post.id}`} className="block text-sm text-muted-foreground hover:underline">
-                    <Time time={post.createdAt} relative={relative} />
-                </Link>
-            </div>
+            <Link
+              href={`/posts/${post.id}`}
+              className="block text-sm text-muted-foreground hover:underline"
+            >
+              <Time time={post.createdAt} relative={relative} />
+            </Link>
+          </div>
         </div>
-        <div className="whitespace-pre-line break-words">
-            <p>{post.content}</p>
-        </div>
+        {post.user.id === user.id && (
+          <PostMoreButton
+            post={post}
+            className="opacity-0 transition-opacity group-hover/post:opacity-100"
+          />
+        )}
+      </div>
+      <div className="whitespace-pre-line break-words">
+        <p>{post.content}</p>
+      </div>
     </article>
-};
+  );
+}
